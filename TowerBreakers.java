585b7ad503f49ee07987a22366eff4049a161ad6
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2024-10-10 12:40:17 +0300

    Create TowerBreakers.java

---
+++ TowerBreakers.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int towerBreakers(int n, int m) {
+        // If all towers are of height 1,
+        // Player 2 wins since Player 1 can't make any moves
+        if (m == 1) {
+            return 2;
+        }
+        
+        // If the number of towers is even,
+        // Player 2 wins by mirroring Player 1's moves
+        if (n % 2 == 0) {
+            return 2;
+        }
+        
+        // If the number of towers is odd,
+        // Player 1 can make the first move and win
+        return 1;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int n = Integer.parseInt(firstMultipleInput[0]);
+
+                int m = Integer.parseInt(firstMultipleInput[1]);
+
+                int result = Result.towerBreakers(n, m);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
